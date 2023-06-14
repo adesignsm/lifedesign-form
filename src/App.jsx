650320020a692d77756b8cda33c84970098b44e0
Akash Mulye <akashmulye97@gmail.com>
2023-06-14 17:35:54 -0400

    added frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./root.css";
 
 import PersonalForm from "./Components/PersonalForm";
+import PopUps from "./Components/PopUps";
+import LD_FLOWER from "./Assets/Media/LD_FLOWER.png";
 
 const App = () => {
     return (
         <>
+            <img id="LD_FLOWER" src={LD_FLOWER} />
             <div id="app">
                 <PersonalForm />
+                <PopUps />
             </div>
         </>
     )
--- src/Components/PersonalForm/index.jsx
+++ src/Components/PersonalForm/index.jsx
@@ -10,6 +10,35 @@ import { useForm } from "react-hook-form";
 
 const app = initializeApp(firebaseClient);
 
+/*
+    capitilize everything except for the "i"'s
+
+    NAME:  
+    ADDRESS:
+    PHONE NUMBER: 
+    EMAIL:
+    BIRTHDAY:
+    PRONOUNS:
+    NATIONALITY:
+
+
+    HEAD SIZE:
+    SHIRT SIZE:
+    PANT SIZE:
+    SHOE SIZE:
+    FAVOURITE COLOUR:
+    LIFE DESIGN NUMBER: 
+    COLORS: 
+
+
+    LABELS: YELLOW WITH A BLUE TEXT STROKE
+    PACEHOLDER: SMOKEY GREY
+
+    PRODUCT PICS FALLING DOWN
+    PICTURES OF FEEF POPPING UP SIMILAR TO BBS DINER
+
+*/
+
 const PersonalForm = () => {
     const [ldNumber, setLdNumber] = useState("");
     const [serverStatus, setServerStatus] = useState("");
@@ -34,7 +63,7 @@ const PersonalForm = () => {
                 for (const data in dataSnapshot) {
                     if (ldNumber !== data) {
                         writeToFirebase(_formData);
-                        setServerStatus(`Thank you Life designer #${ldNumber} your account has been submitted!`);
+                        setServerStatus(`Welcome Life designer #${ldNumber}!`);
                         $("#server-status-container").fadeIn(500);
                         setTimeout(() => {
                             $("#server-status-container").fadeOut(500);
@@ -69,68 +98,72 @@ const PersonalForm = () => {
                     validateLdNumber(data)
                 })}>
                     {/*NAME INPUT */}
-                    <label htmlFor="user-name">Name: </label>
-                    <input {...register('user-name')} name="user-name" placeholder="Please enter your name" required/>
+                    <label htmlFor="user-name">NAME: </label>
+                    <input {...register('user-name')} name="user-name" placeholder="XYLK AKA PiGO THE PiNOY NiGO AKA FLEONARDO DiCAPRiO" required/>
 
                     {/*ADDRESS INPUT */}
-                    <label htmlFor="user-address">Address: </label>
-                    <input {...register('user-address')} name="user-address" placeholder="123 starroad dr." required/>
+                    <label htmlFor="user-address">ADDRESS: </label>
+                    <input {...register('user-address')} name="user-address" placeholder="MANiLLA PHiLLiPiNES" required/>
 
                     {/*PHONE NUMBER INPUT */}
-                    <label htmlFor="user-phone-number">Phone Number: </label>
-                    <input {...register('user-phone-number')} name="user-phone-number" placeholder="8888888888" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" maxLength={12} required/>
+                    <label htmlFor="user-phone-number">PHONE NUMBER: </label>
+                    <input {...register('user-phone-number')} name="user-phone-number" placeholder="555-308-1313" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" maxLength={12} required/>
 
                     {/*EMAIL INPUT */}
-                    <label htmlFor="user-email">Email: </label>
-                    <input {...register('user-email')} type="email" name="user-email" placeholder="Please enter your email" required/>
+                    <label htmlFor="user-email">EMAiL: </label>
+                    <input {...register('user-email')} type="email" name="user-email" placeholder="ASK@XYLK.CO" required/>
                     
                     {/*BIRTHDAY INPUT*/}
-                    <label for="user-birthday">Birthday: </label>
-                    <input {...register('user-birthday')} type="date" id="birthday" name="user-birthday" required />
-
-                    {/*AGE INPUT */}
-                    <label htmlFor="user-age">Age: </label>
-                    <input {...register('user-age')} name="user-age" pattern="[0-9]{1}[0-9]{1}" maxLength={2} placeholder="88" required/>
+                    <label for="user-birthday">BiRTHDAY: </label>
+                    <input {...register('user-birthday')} type="date" id="birthday" name="user-birthday" placeholder="EVERYDAY" required />
                 
                     {/*PRONOUNS INPUT*/}
-                    <label for="user-pronouns">Pronouns:</label>
-                    <select {...register('user-pronouns')} id="pronouns" name="user-pronouns" required>
-                        <option value="he/him">He/Him</option>
-                        <option value="she/her">She/Her</option>
-                        <option value="they/them">They/Them</option>
-                        <option value="other">Other</option>
+                    <label for="user-pronouns">PRONOUNS:</label>
+                    <select {...register('user-pronouns')} id="pronouns" name="user-pronouns" placeholder="HE/HiM" required>
+                        <option value="he/him">HE<span className="unique-characters">/</span>HiM</option>
+                        <option value="she/her">SHE<span className="unique-characters">/</span>HER</option>
+                        <option value="they/them">THEY<span className="unique-characters">/</span>THEM</option>
+                        <option value="other">OTHER</option>
                     </select>
 
                     {/*NATIONALITY INPUT*/}
-                    <label for="user-nationality">Nationality:</label>
-                    <input type="text" {...register("user-nationality")} id="user-nationality" name="user-nationality" placeholder="Canadian" required />
+                    <label for="user-nationality">NATiONALiTY:</label>
+                    <input type="text" {...register("user-nationality")} id="user-nationality" name="user-nationality" placeholder="PiNOY TO THE BONE" required />
 
                     <div id="break-line"></div>
                     {/*HEAD SIZE INPUT */}
-                    <label htmlFor="user-head-size">Head Size: </label>
-                    <input {...register('user-head-size')} name="user-head-size" placeholder="please indicate in inches" required/>
+                    <label htmlFor="user-head-size">HEAD SiZE: </label>
+                    <input {...register('user-head-size')} name="user-head-size" placeholder="TOO BiG LiKE WATER" required/>
 
                     {/*SHIRT SIZE INPUT */}
-                    <label htmlFor="user-shirt-size">Shirt Size: </label>
-                    <input {...register('user-shirt-size')} name="user-shirt-size" placeholder="small, medium, etc." required/>
+                    <label htmlFor="user-shirt-size">SHiRT SiZE: </label>
+                    <input {...register('user-shirt-size')} name="user-shirt-size" placeholder="MEJiOOM" required/>
 
                     {/*PANTS SIZE INPUT */}
-                    <label htmlFor="user-pants-size">Pants Size: </label>
-                    <input {...register('user-pants-size')} name="user-pants-size" placeholder="small, medium, etc." required/>
+                    <label htmlFor="user-pants-size">PANTS SiZE: </label>
+                    <input {...register('user-pants-size')} name="user-pants-size" placeholder="i PREFER NOT TO SAY" required/>
 
                     {/*SHOE SIZE INPUT */}
-                    <label htmlFor="user-shoe-size">Shoe Size: </label>
-                    <input {...register('user-shoe-size')} name="user-shoe-size" placeholder="xx" required/>
+                    <label htmlFor="user-shoe-size">SHOE SiZE: </label>
+                    <input {...register('user-shoe-size')} name="user-shoe-size" placeholder="13.5" required/>
                     
                     {/*FAVOURITE COLOUR INPUT */}
-                    <label htmlFor="user-favourite-color">Favourite Colour: </label>
-                    <input {...register('user-favourite-color')} name="user-favourite-color" placeholder="red, blue, etc." required/>
+                    <label htmlFor="user-favourite-color">FAVOURiTE COLOR: </label>
+                    <input {...register('user-favourite-color')} name="user-favourite-color" placeholder="LiGHT TAN" required/>
                     
                     {/*LD NUMBER INPUT */}
-                    <label htmlFor="user-lifedesigner-number">Life Designer Number: </label>
-                    <input type="text" {...register('user-lifedesigner-number')} name="user-lifedesigner-number" placeholder="222" onChange={(e) => {setLdNumber(e.target.value)}} required/>
+                    <label htmlFor="user-lifedesigner-number">LiFE DESiGN: </label>
+                    <input type="text" {...register('user-lifedesigner-number')} name="user-lifedesigner-number" placeholder="JUAN" onChange={(e) => {setLdNumber(e.target.value)}} required/>
+                    <div id="break-line"></div>
+
+                    {/*PRONOUNS INPUT*/}
+                    <label for="user-subscribe">WOULD YOU SUBSCRiBE<span className="unique-characters">?</span> OFC</label>
+                    <select {...register('user-subscribe')} id="subscribe" name="user-subscribe" required>
+                        <option value="he/him">YES</option>
+                        <option value="she/her">NO</option>
+                    </select>
                     
-                    <input className="submit-button" type="submit" />
+                    <input className="submit-button" type="submit" value="SiGN ME UP"/>
                 </form>
             </div>
             {showServerStatus === true ? <ServerStatus data={serverStatus} /> : null}
